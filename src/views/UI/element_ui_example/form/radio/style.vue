75e0cb19d0ff9f76e270a552aa9c82c1031c2ad7
darren4neil <liguangfenghappy@163.com>
2023-05-22 17:30:13 +0800

    单选样式

---
+++ src/views/UI/element_ui_example/form/radio/style.vue
@@ -0,0 +1,26 @@
+<template>
+    <page-main>
+        <div v-for="(item, index) in styles" :key="index" style="margin: 30px;">
+            <el-radio-group v-model="radio1" :index="index" :size="item">
+                <el-radio-button label="上海"></el-radio-button>
+                <el-radio-button label="北京"></el-radio-button>
+                <el-radio-button label="广州"></el-radio-button>
+                <el-radio-button label="深圳"></el-radio-button>
+            </el-radio-group>
+        </div>
+    </page-main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            styles: ['', 'medium', 'small', 'mini'],
+            radio1: '上海',
+            radio2: '上海',
+            radio3: '上海',
+            radio4: '上海'
+        }
+    }
+}
+</script>
